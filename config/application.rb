3bfb6e08549dc452cf4ae3e88a136d64196f8bdd
Brian Turnbull <post@brianturnbull.com>
2013-09-08 12:39:16 -0400

    Set HAML as template engine

--- config/application.rb
+++ config/application.rb
@@ -63,6 +63,7 @@ module Orstrings
     config.generators do |g|
       g.test_framework      :rspec, :views => false, :fixture => true
       g.fixture_replacement :factory_girl, :dir => 'spec/factories'
+      g.template_engine     :haml
     end
 
   end
