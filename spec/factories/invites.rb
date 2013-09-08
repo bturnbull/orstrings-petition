e4d5649f30e62db0d0c8f2e1db94bb469f4a3dcc
Brian Turnbull <post@brianturnbull.com>
2013-09-08 17:07:20 -0400

    Factories

--- spec/factories/invites.rb
+++ spec/factories/invites.rb
@@ -1,6 +1,7 @@
-# Read about factories at https://github.com/thoughtbot/factory_girl
-
 FactoryGirl.define do
   factory :invite do
+    association :sender,    :factory => :petitioner
+    association :recipient, :factory => :petitioner
+    sent_at DateTime.now
   end
 end
--- spec/factories/petitioners.rb
+++ spec/factories/petitioners.rb
@@ -1,6 +1,40 @@
-# Read about factories at https://github.com/thoughtbot/factory_girl
-
 FactoryGirl.define do
-  factory :petitioner do
+  factory :petitioner, :aliases => [:sender, :recipient] do
+    sequence(:email) {|n| "joe.user#{n}@example.com"}
+    first_name 'Joe'
+    last_name 'User'
+    town 'Durham'
+    is_visible true
+    can_email true
+    signing_token {Digest::MD5.hexdigest(Time.now.to_s)}
+    signed_at DateTime.now
+
+    trait :private do
+      is_visible false
+    end
+
+    trait :uninterested do
+      can_email false
+    end
+
+    trait :unsigned do
+      signed_at nil
+    end
+
+    factory :petitioner_with_invites do
+      ignore do
+        invite_count 3
+      end
+
+      after(:create) do |sender, evaluator|
+        FactoryGirl.create_list(:invite, evaluator.invite_count, :sender => sender)
+      end
+    end
+
+    factory :invited_petitioner do
+      after(:create) do |recipient, evaluator|
+        FactoryGirl.create(:invite, :recipient => recipient)
+      end
+    end
   end
 end
