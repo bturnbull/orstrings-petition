6eddee87f150b7a5479458c8a29a51c31a051499
Brian Turnbull <post@brianturnbull.com>
2013-09-11 12:51:38 -0400

    Minimal Petitioners controller

--- app/controllers/petitioners_controller.rb
+++ app/controllers/petitioners_controller.rb
@@ -1,13 +1,23 @@
 class PetitionersController < ApplicationController
   def index
+    @petitioners = Petitioner.all
   end
 
   def show
+    @petitioner = Petitioner.find(params[:id])
   end
 
   def new
+    @petitioner = Petitioner.new
   end
 
   def create
+    @petitioner = Petitioner.new(params[:petitioner])
+
+    if @petitioner.save
+      redirect_to petition_signer_url(@petitioner)
+    else
+      render :action => 'new'
+    end
   end
 end
