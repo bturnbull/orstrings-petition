5eb5d03d6bf21896f6d09b630cca37866f177612
Brian Turnbull <post@brianturnbull.com>
2013-09-22 09:53:28 -0400

    Friendly times helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+
+  def friendly_time(time)
+    if time > 90.seconds.ago
+      'just now'
+    elsif time > 12.hours.ago
+      'about ' + time_ago_in_words(time) + ' ago'
+    elsif time > 3.days.ago
+      time.strftime('on %A around %l %p')
+    else
+      time.strftime('on %A, %B %-d around %l %p')
+    end
+  end
 end
---
+++ spec/helpers/application_helper_spec.rb
@@ -0,0 +1,23 @@
+require 'spec_helper'
+
+describe ApplicationHelper do
+
+  describe 'friendly_time' do
+
+    it 'should return "just now" for near past times' do
+      friendly_time(60.seconds.ago).should eq('just now')
+    end
+
+    it 'should return time_ago_in_words for not as near past times' do
+      friendly_time(10.minutes.ago).should eq('about ' + time_ago_in_words(10.minutes.ago) + ' ago')
+    end
+
+    it 'should return day of week and time for couple days past times' do
+      friendly_time(2.days.ago).should eq(2.days.ago.strftime('on %A around %l %p'))
+    end
+
+    it 'should return day of week, date, and time for older past times' do
+      friendly_time(7.days.ago).should eq(7.days.ago.strftime('on %A, %B %-d around %l %p'))
+    end
+  end
+end
