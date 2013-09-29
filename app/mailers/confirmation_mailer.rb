94cc281329df88ab83855bc97489644e75710c47
Brian Turnbull <post@brianturnbull.com>
2013-09-29 12:30:02 -0400

    Fancier confirmation mail

--- app/mailers/confirmation_mailer.rb
+++ app/mailers/confirmation_mailer.rb
@@ -2,11 +2,11 @@ class ConfirmationMailer < MailerBase
 
   # Mail to confirm a petiton signature.  TODO - template for mail body
   def confirmation(confirmation)
-    unless confirmation.confirmed?
-      mail :subject => "OR Strings Petition Signature Confirmation",
-           :to      => confirmation.signature.email,
-           :from    => "OR Strings Petition <confirm@orstrings.org>",
-           :body    => "#{confirmation.signature.first_name} -- Please click this link to confirm your signature. #{petition_confirmation_url(confirmation)}"
+    @confirmation = confirmation
+    unless @confirmation.confirmed?
+      mail(:subject => "OR Strings Petition Signature Confirmation",
+           :to      => @confirmation.signature.email,
+           :from    => "OR Strings Petition <confirm@orstrings.org>")
     end
   end
 
